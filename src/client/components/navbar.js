fc13be9d7134d1cdc793526b41d7a954d78e43f4
colev1 <colevanacore@gmail.com>
2019-05-09 21:04:24 -0400

    Add styling to selected nav bar link

--- src/client/components/navbar.js
+++ src/client/components/navbar.js
@@ -28,55 +28,36 @@ const defaultProps = {
  * Navbar component
  * @returns {ReactElement}
  */
-const Navbar = ({ filterBy, onClickFilter }) => {
+const Navbar = () => {
   /**
    * Base CSS class
    */
-  const baseCls = 'navbar'
-
-  // let activeLinkCls = `${baseCls}__item`;
-  // activeLinkCls += filterBy === 'active' ? ` ${baseCls}__item--active` : '';
-
-  // let completedLinkCls = `${baseCls}__item`;
-  // completedLinkCls += filterBy === 'completed' ? ` ${baseCls}__item--active` : '';
-
-  // let archivedLinkCls = `${baseCls}__item`;
-  // completedLinkCls += filterBy === 'archived' ? ` ${baseCls}__item--active` : '';
 
   return (
-    <div className={baseCls}>
-      <div>
+    <div>
+      <div className = "navbar"> 
         <NavLink
-          to="/"
-        // activeClassName={`${baseCls}__item--active`}
-        // className={`${baseCls}__item`}
-        // onClick={() => onClickFilter('')}
+          exact to="/"       
         >
           All
         </NavLink>
         <NavLink
           to="/active"
-        // className={activeLinkCls}
-        // onClick={() => onClickFilter('active')}
         >
           Active
         </NavLink>
         <NavLink
           to="/complete"
-        // className={completedLinkCls}
-        // onClick={() => onClickFilter('completed')}
         >
           Completed
         </NavLink>
         <NavLink
           to="/archive"
-        // className={archivedLinkCls}
-        // onClick={() => onClickFilter('archived')}
         >
           Archived
         </NavLink>
       </div>
-      <Button text="Archive all completed" />
+      <Button text= "Archive all completed" />
     </div>
   );
 }
